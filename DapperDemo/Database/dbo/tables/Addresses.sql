9465205a28848d7a81b6ed7e60a42b3eceef31ef
Rajan-abjima <125352988+Rajan-abjima@users.noreply.github.com>
2023-05-25 19:09:50 +0530

    25-05-23

---
+++ DapperDemo/Database/dbo/tables/Addresses.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Addresses]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+    [ContactId] INT NOT NULL, 
+    [AddressType] NCHAR(10) NOT NULL, 
+    [StreetAddress] NCHAR(50) NOT NULL, 
+    [City] NCHAR(50) NOT NULL, 
+    [StateId] INT NOT NULL, 
+    [PostalCode] NCHAR(20) NOT NULL, 
+    CONSTRAINT [PK_Addresses] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Addresses_Contacts] FOREIGN KEY([ContactId]) REFERENCES Contacts(Id),
+    CONSTRAINT [FK_Addresses_States] FOREIGN kEY([StateId]) REFERENCES States(Id)
+)
---
+++ DapperDemo/Runner/Program.cs
@@ -0,0 +1,42 @@
+﻿using DataLayer;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace Runner
+{
+    internal class Program
+    {
+        private static IConfigurationRoot config;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void Get_all_should_return_6_results()
+        {
+            //arrange
+            var repository = CreateRepository();
+
+            //act
+            var Contacts = repository.GetAll();
+
+            //asset
+            Console.WriteLine($"Count: {Contacts.Count}");
+            Debug.Assert( Contacts.Count == 6 );
+            Contacts.Output();
+        }
+
+        private static void Initialize()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettigns.json", optional: true, reloadOnChange: true);
+            config = builder.Build();
+        }
+
+        private static IContactRepository CreateRepository()
+        {
+            return new ContactRepository (config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
